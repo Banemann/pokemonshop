148e9aee8fb7ae10635a9bc477327dad77a03db8
Casper Banemann <casperbanemann@gmail.com>
2024-09-18 16:34:11 +0200

    struktur

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <p>&copy; 2024 Anders' Tømrer- og Malerservice.</p>
-        <p>Telefon: +45 1234 5678</p>
-        <p>E-mail:</p>
+        <p>&copy; 2024 SkovlundeByg Tømrer- og Malerservice.</p>
+        <p>&#9742; (+45) 29 70 91 94</p> 
+        <p>&#9993; skovlundebyg@gmail.com</p> 
       </div>
     </footer>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,12 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="home-page">
+    <main className="home-page">
       <header className="hero-section">
         <div className="overlay">
-          <h1 className='homeh1'>Vi er SkovlundeByg <br></br> Din lokale partner til alt inden for tømrer- og malerarbejde.</h1>
-          <h2 className='homeh2'>Håndværk med Kvalitet og Personlig Service</h2>
-          
+          <h1 className='homeh1'><span className='underline'>SkovlundeByg</span> <br /> Din lokale partner til alt inden for tømrer- og malerarbejde.</h1>
           <p className='homep'>
-            Med base i Skovlunde har vi hjulpet lokale kunder med deres byggeprojekter. <br></br> Vi specialiserer os i alt fra tømrerarbejde til maleropgaver og leverer resultater af højeste kvalitet.
+            Med base i Skovlunde har vi hjulpet lokale kunder med deres byggeprojekter. Vi specialiserer os i alt fra tømrerarbejde til maleropgaver og leverer resultater af højeste kvalitet.
           </p>
           <a href="/kontakt" className="cta-button" aria-label="Contact us for a quote">
             Kontakt os i dag for et uforpligtende tilbud!
@@ -18,28 +16,35 @@ function Home() {
         </div>
       </header>
 
-      
-
-      <section className="textcontent">
-        <h2 className='homeh2'>Tømrer- & Malerservice i Skovlunde</h2>
-        <h3 className='homeh3'>Vi tilbyder en bred vifte af tømrer- og malerydelser...</h3>
-
-        <h4 className='homeh4'>Tømrerarbejde</h4>
-        <p className='homep'>
-          Fra nye terrasser til indvendige gulve og køkkeninstallationer, vores arbejde er præget af omhu og kvalitetsmaterialer.
-        </p>
-        
-        <h4 className='homeh4'>Malerarbejde</h4>
-        <p className='homep'>
-          Vi hjælper med alt fra maling af vægge til udvendige facader, så dit hjem kan få det friske look, det fortjener.
-        </p>
+      <section className="introcontent">
+        <h2 className='homeh2'>Håndværk med Kvalitet og Personlig Service</h2>
+        <p className='homep'>Vi leverer håndværk med fokus på detaljer og kundetilfredshed i Skovlunde og omegn.</p>
       </section>
       
       <section className="imgcontent">
-      <img src="/anders2.jpg" alt="Example" />
-      <img src="/anders3.jpg" alt="Example" />
+        <img src="/anders2.jpg" alt="Tømrerarbejde eksempel fra SkovlundeByg" />
+        <img src="/anders3.jpg" alt="Malerservice eksempel fra SkovlundeByg" />
       </section>
-    </div>
+
+      <section className="servicecontent">
+        <h2 className='homeh2'>Tømrer- & Malerservice i Skovlunde</h2>
+        <h3 className='homeh3'>Vi tilbyder en bred vifte af tømrer- og malerydelser...</h3>
+
+        <article>
+          <h4 className='homeh4'>Tømrerarbejde</h4>
+          <p className='homep'>
+            Fra nye terrasser til indvendige gulve og køkkeninstallationer, vores arbejde er præget af omhu og kvalitetsmaterialer.
+          </p>
+        </article>
+
+        <article>
+          <h4 className='homeh4'>Malerarbejde</h4>
+          <p className='homep'>
+            Vi hjælper med alt fra maling af vægge til udvendige facader, så dit hjem kan få det friske look, det fortjener.
+          </p>
+        </article>
+      </section>
+    </main>
   );
 }
 
