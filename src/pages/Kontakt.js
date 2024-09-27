0c2a7c84107d29e9a77712e20882d6c381ae1a8b
Casper Banemann <casperbanemann@gmail.com>
2024-09-27 16:39:45 +0200

    kontaktside

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Kontakt.css';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px',
+};
+
+const center = {
+  lat: 55.7308,
+  lng: 12.4066, 
+};
 
 function Kontakt() {
   return (
     <div className="contact-page">
+      <div>
       <h1 className='kontakth1'>Kontakt SkovlundeByg</h1>
       <h2 className='kontakth2'>
         Har du spørgsmål eller brug for et tilbud? Vi er klar til at hjælpe dig med dit næste byggeprojekt. 
@@ -12,7 +24,7 @@ function Kontakt() {
       <address className='kontaktaddress'>
         <p className='kontaktp'>
           <span>Telefon: </span>
-          <a className='kontakta' href="tel:+4512345678">+45 1234 5678</a>
+          <a className='kontakta' href="tel:+4512345678">(+45) 29 70 91 94</a>
         </p>
         <p className='kontaktp'>
           <span>E-mail: </span>
@@ -25,6 +37,32 @@ function Kontakt() {
           <span>Åbningstider: Man-Fre: 08.00-16.00</span>
         </p>
       </address>
+
+      <form className='kontaktform'>
+        <label className='kontaktlabel' htmlFor="name">Navn:</label>
+        <input className='kontaktinput' type="text" id="name" name="name" required />
+
+        <label className='kontaktlabel' htmlFor="email">E-mail:</label>
+        <input className='kontaktinput' type="email" id="email" name="email" required />
+
+        <label className='kontaktlabel' htmlFor="phone">Telefon:</label>
+        <input className='kontaktinput' type="tel" id="phone" name="phone" required />
+
+        <label className='kontaktlabel' htmlFor="message">Besked:</label>
+        <textarea className='kontakttextarea' id="message" name="message" required></textarea>
+
+        <button className='kontaktbutton' type="submit">Send</button>
+      </form>
+
+
+      <div className="map-container">
+        <LoadScript googleMapsApiKey="AIzaSyCMP4bFc7f3vvbXOwg1osq0e992TtzQTC4">
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+            <Marker position={center} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
+      </div>
     </div>
   );
 }
