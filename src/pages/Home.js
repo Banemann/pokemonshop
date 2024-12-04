6dabb2bffd6344e929006f9aec30cd4946cc5ddd
Casper Banemann <casperbanemann@gmail.com>
2024-12-04 14:35:42 +0100

    body html margin padding

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div className='homediv'>
-     
-      
+    <div className="homediv">
+
+      <div className="homenyheder">
+        <h1>Populære Nyheder</h1>
+        <p>
+          En elektrisk bølge er på vej med{" "}
+          <strong>Scarlet and Violet Surging Sparks</strong>, oplev også den nye{" "}
+          <strong>Pokémon julekalender</strong>.
+        </p>
+        <div className="nyhederimagediv">
+          <img src="nyhed1.png" alt="nyhed1" />
+          <img src="nyhed2.png" alt="nyhed2" />
+        </div>
+      </div>
 
     </div>
   );
